da40db7bb338e3f282dd5bbad2b16d0f549d88f6
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-13 10:54:08 +0100

    completed  add method

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,4 +7,10 @@ describe Calculator do
     it "responds to add with 2 args" do 
     expect(subject).to respond_to(:add).with(2).arguments
     end   
+    it "gives 2 when passed (1,2)" do 
+     expect(subject.add(1,2)).to eq 3
+    end
+    it "gives 2 when passed (1,1)" do 
+        expect(subject.add(1,1)).to eq 2
+       end  
 end 
