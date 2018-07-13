36e47261bfba4e14e6d5000ec7bd6a25d7a16e7c
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-07-13 15:28:55 +0100

    progress before pairing

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,16 +1,13 @@
-class Calculator 
-  
-    def add(a,b)
-    (a+b)
-  end 
-def pretty_add(a,b)
-"The sum of #{a} and #{b} is #{add(a,b)}"
-end 
- 
- def add_many(array)
- 
-  array.sum 
-  
- end
+class Calculator
+  def add(a, b)
+    (a + b)
+end
 
+  def pretty_add(a, b)
+    "The sum of #{a} and #{b} is #{add(a, b)}"
+  end
+
+  def add_many(array)
+    array.sum
+  end
 end
